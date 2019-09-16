ce7ceceee013b381d5771c427dd9220263e7cf03
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-09-16 12:33:25 +0100

    Added jump functionality to palyer

--- New Unity Project 2D/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project 2D/Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,21 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate ()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }

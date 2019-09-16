3ef94b1ab28e443887f4166a3dc7efa30c8bb2cc
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-09-16 15:26:30 +0100

    Charater Movement complete, with crouch

--- New Unity Project 2D/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project 2D/Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,48 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
+    bool crouch = false;
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Isjumping", true);
         }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
+    }
+
+    public void OnLanding () 
+    {
+        animator.SetBool("Isjumping", false);
+    }
+
+    public void OnCrouching (bool isCrouching)
+    {
+        animator.SetBool("Iscrouching", isCrouching);
     }
 
     void FixedUpdate ()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+        
     }
 }

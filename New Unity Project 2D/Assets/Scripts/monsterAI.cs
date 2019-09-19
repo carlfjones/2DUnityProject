b2f9a9af2446279ac9d54e7acfefdad4147d62f8
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-09-19 17:29:00 +0100

    Updated animations for Blue Monster

---
+++ New Unity Project 2D/Assets/Scripts/monsterAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterAI : MonoBehaviour
+{
+    // Integers
+    public int curHealth;
+    public int maxHealth;
+
+    //Floats
+    public float distance;
+    public float wakeRange;
+    public float shootInterval;
+    public float bulletSpeed = 100;
+    public float bulletTimer;
+
+    //Booleans
+    public bool awake = false;
+    public bool lookingRight = true;
+
+    //References
+    public GameObject bullet;
+    public Transform target;
+    public Animator anim;
+    public Transform shootPointLeft;
+    public Transform shootPointRight;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        anim.SetBool("Awake", awake);
+        anim.SetBool("LookingRight", lookingRight);
+
+        RangeCheck();
+
+    }
+
+    void RangeCheck()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+
+        if(distance < wakeRange)
+        {
+            awake = true;
+        }
+
+        if (distance > wakeRange)
+        {
+            awake = false;
+        }
+    }
+
+}
